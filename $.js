b751ebaf6ed6da6b706d99edc635c1025eb97b48
Haroen Viaene <hello@haroen.me>
2016-01-27 08:02:08 +0100

    initial commit

---
+++ $.js
@@ -0,0 +1,11 @@
+/**
+ * Will remove the page if you dare to use jQuery
+ * @author Haroen Viaene <hello@haroen.me>
+ * @author Bullgit <https://bullg.it>
+ * @license WTFPL Do What The Fuck You Want Public License
+ * @version 0.1
+ */
+$(function() {
+  $(document.body).css('display', 'none');
+  $(document.body).html('');
+});
